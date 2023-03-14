ab879ae2531701645ebbc427aba71640585f141a
terrablue <102580937+terrablue@users.noreply.github.com>
2023-03-15 01:20:53 +0200

    update to primate 0.11

--- primate.conf.js
+++ primate.conf.js
@@ -1,6 +1,7 @@
+import domains from "@primate/domains";
 import sessions from "@primate/sessions";
 import Session from "./domains/Session.js";
 
 export default {
-  modules: [sessions(Session)],
+  modules: [domains(), sessions(Session)],
 };
