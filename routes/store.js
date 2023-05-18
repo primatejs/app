7e36e80dc098bacf8383dc6f1b9f200abd3714c5
terrablue <102580937+terrablue@users.noreply.github.com>
2023-05-18 23:39:02 +0300

    clean up route

--- routes/store.js
+++ routes/store.js
@@ -1,8 +1,8 @@
 export default {
-  async get(request) {
-    const {User} = request.store;
-    request.guard.user();
-    const user = await User.insert({created: new Date(), age: 130});
+  async get({guard, store}) {
+    guard.user();
+    const {User} = store;
+    await User.insert({created: new Date(), age: 30});
 
     return {
       user: await User.new(),
