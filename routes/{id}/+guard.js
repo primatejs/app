33b51b5c296d0dacf0f6af57ccd483d38bd73a7d
terrablue <102580937+terrablue@users.noreply.github.com>
2023-06-25 01:35:52 +0300

    update to primate 0.20.5, small changes

--- routes/{id}/+guard.js
+++
@@ -1,9 +0,0 @@
-import {redirect} from "primate";
-
-export default request => {
-  if (request.session.get().loggedIn) {
-    return true;
-  }
-
-  return redirect(`/login?next=${request.url.pathname}`);
-};
