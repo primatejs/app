561c1649ce49bd7aba7929eb27ef9cf17b1ee8ce
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-11 19:46:50 +0300

    upgrade @primate/react to 0.8.1

--- components/PostIndex.jsx
+++ components/PostIndex.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-export default class extends React.Component {
+/*export default class extends React.Component {
   render() {
-    const {posts} = this.props;
+    const {posts} = this.props.data;
 
     return (<>
-      <h1>All posts</h1>
+      <h1 onClick={() => {console.log("HI!")}}>All posts</h1>
       {posts.map(({id, title}) => (
         <h2><a href={`/react/post/view/${id}`}>{title}</a></h2>
       ))}
       <h3><a href="/react/post/edit/">add post</a></h3>
     </>);
   }
-}
+}*/
+
+export default ({data: {posts}}) =>
+  (<>
+    <h1 onClick={() => {console.log("HI!")}}>All posts</h1>
+    {posts.map(({id, title}) => (
+      <h2><a href={`/react/post/view/${id}`}>{title}</a></h2>
+    ))}
+    <h3><a href="/react/post/edit/">add post</a></h3>
+  </>);
