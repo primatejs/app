e6029a2f3df3e10462dfcc0f9eec14d78e45e946
phaleth <29838606+phaleth@users.noreply.github.com>
2024-02-03 15:41:43 +0100

    update comment in users service

--- services/users.js
+++ services/users.js
@@ -1,10 +1,10 @@
 /*
- * By adding the directory `services` to build.includes, any files here,
- * including this one, will included in the build and be accessible to any
- * JavaScript component for importing.
+ * By adding the directory `services` to build.includes in `primate.config.js`,
+ * any files here, including this one, will be included in the build and be
+ * available to any JavaScript component for import.
  *
  * This is useful if you want to fetch data from Primate (or any other source)
- * dynamically, using `fetch` or any other method.
+ * dynamically, using `fetch` or any other http client.
  *
  * See `components/svelte/PostIndex.svelte` for a typical import of such an
  * include.
